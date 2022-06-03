052ecdac6e0f5080afc9060fd1d3416fac5f7f87
ishinotakkyuu <tanktopgentleman.secondseason@gmail.com>
2022-06-03 23:10:05 +0900

    開発を進めました

--- app/src/main/java/com/example/title_1/DataDetail.java
+++ app/src/main/java/com/example/title_1/DataDetail.java
@@ -5,13 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 public class DataDetail extends AppCompatActivity {
-
-    TextView textView;
-    String ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +23,7 @@ public class DataDetail extends AppCompatActivity {
         // 個別データ画面から引き継がれてきたIDを受け取る
         Intent intent = getIntent();
         // 受け取ったデータを取り出す
-        ID = intent.getStringExtra("ID");
+        String ID = intent.getStringExtra("ID");
 
 
 
@@ -36,21 +32,8 @@ public class DataDetail extends AppCompatActivity {
 
 
         // 取得したデータを各TextViewにセット(R04.06.01時点では仮として、IDのみをセットする)
-        textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textView);
         textView.setText(ID);
 
     }
-
-    public void takeOverID(View view){
-        // MainCounterActivityを遷移先にセット
-        Intent intent = new Intent(this,MainCounterActivity.class);
-        // Intentに引き継ぐIDをセット
-        intent.putExtra("ID",ID);
-        // MainCounterActivityに遷移させる
-        startActivity(intent);
-
-    }
-
-
-
 }
