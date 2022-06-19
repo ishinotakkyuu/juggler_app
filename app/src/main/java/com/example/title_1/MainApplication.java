b17a6c87cd40f28cd27e4e76fb785060241af78b
ishinotakkyuu <tanktopgentleman.secondseason@gmail.com>
2022-06-20 01:25:38 +0900

    整理整頓

--- app/src/main/java/com/example/title_1/MainApplication.java
+++ app/src/main/java/com/example/title_1/MainApplication.java
@@ -47,7 +47,7 @@ public class MainApplication extends Application {
     public void init() {
         Random rnd = new Random();
         //乱数を取得する
-        if(userId != 0){
+        if(userId == 0){
             setUserId(rnd.nextInt(2147483646)+1);
         }
         setTotalGames("0");
