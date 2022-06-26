d2872ef17baa76e668598e0c899c86c2d84e96bd
ishinotakkyuu <tanktopgentleman.secondseason@gmail.com>
2022-06-26 16:44:59 +0900

    店舗管理にメモ機能の追加実装(とりあえず完成)

--- app/src/main/java/com/example/title_1/MainManagementStoreMemo.java
+++ app/src/main/java/com/example/title_1/MainManagementStoreMemo.java
@@ -1,18 +1,22 @@
 package com.example.title_1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.widget.ArrayAdapter;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import org.apache.commons.lang3.StringUtils;
 
 public class MainManagementStoreMemo extends AppCompatActivity implements TextWatcher {
 
@@ -25,6 +29,13 @@ public class MainManagementStoreMemo extends AppCompatActivity implements TextWa
     // 各種メモTextやXMLを格納する配列
     String[] memo, memoTagNames;
 
+    // フォーカス
+    ConstraintLayout memoLayout;
+    InputMethodManager inputMethodManager;
+
+    //
+    boolean menuFlag = true;
+
     // 共有データ
     MainApplication mainApplication = null;
 
@@ -32,6 +43,9 @@ public class MainManagementStoreMemo extends AppCompatActivity implements TextWa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main01_storemanagement03_memo);
+
+        memoLayout = findViewById(R.id.MemoLayout);
+        inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
         // ストレージの取得
         mainApplication = (MainApplication) this.getApplication();
@@ -41,12 +55,91 @@ public class MainManagementStoreMemo extends AppCompatActivity implements TextWa
 
         // EditTextの設定
         eMemo = findViewById(R.id.StoreMemo);
+        eMemo.setFocusable(false);
+        eMemo.setFocusableInTouchMode(false);
         eMemo.addTextChangedListener(this);
 
         // 受け取ったインデックスを使用して該当のXMLよりメモを取得、EditTextにセット
         Intent intent = getIntent();
         catchTappedPosition = Integer.parseInt(intent.getStringExtra("tappedPosition"));
         setValue(catchTappedPosition);
+    }
+
+    // メニューのセット
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.memo_bar, menu);
+        return true;
+    }
+
+    // フラグ判定でメニュー表示を変更
+    @Override
+    public boolean onPrepareOptionsMenu(@NonNull Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        if(menuFlag) {
+            menu.getItem(1).setVisible(false);
+            menu.getItem(2).setVisible(false);
+        } else {
+            menu.getItem(0).setVisible(false);
+            menu.getItem(1).setVisible(true);
+            menu.getItem(2).setVisible(true);
+        }
+        return true;
+    }
+
+    // 各メニュー選択時の処理振り分け
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.edit:
+
+                // フラグを変更、メニュー表示の更新
+                menuFlag = false;
+                invalidateOptionsMenu();
+
+                // 編集可能にしてフォーカスを文字列の末尾にセットし、キーボードを表示
+                eMemo.setFocusable(true);
+                eMemo.setFocusableInTouchMode(true);
+                String selectionText = eMemo.getText().toString();
+                eMemo.requestFocus();
+                eMemo.setSelection(selectionText.length());
+                inputMethodManager.showSoftInput(eMemo,0);
+                break;
+
+            case R.id.reset:
+
+                new AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.memo_delete_alert))
+                        .setMessage(getString(R.string.memo_delete_alert_message))
+                        .setCancelable(false)
+                        .setPositiveButton(getString(R.string.yes), (dialog, which) -> {
+
+                            eMemo.setText(getString(R.string.default_memo));
+                            memoLayout.requestFocus();
+
+                            Toast toast = Toast.makeText(MainManagementStoreMemo.this, getString(R.string.memo_delete_toast), Toast.LENGTH_LONG);
+                            toast.show();
+
+                        })
+                        .setNegativeButton(getString(R.string.cancel), null)
+                        .show();
+                break;
+
+            case R.id.finish:
+
+                // フラグを変更、メニュー表示の更新
+                menuFlag = true;
+                invalidateOptionsMenu();
+
+                // 編集不可にしてフォーカスを外し、キーボードを閉じる
+                eMemo.setFocusable(false);
+                eMemo.setFocusableInTouchMode(false);
+                inputMethodManager.hideSoftInputFromWindow(memoLayout.getWindowToken(), 0);
+                memoLayout.requestFocus();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void setValue(int catchTappedPosition) {
@@ -69,7 +162,7 @@ public class MainManagementStoreMemo extends AppCompatActivity implements TextWa
     public void afterTextChanged(Editable s) {
 
         final String[] text = {s.toString()};
-        if (text[0].isEmpty()) {
+        if(text[0].isEmpty()) {
             text[0] = "null";
         }
 
@@ -82,22 +175,19 @@ public class MainManagementStoreMemo extends AppCompatActivity implements TextWa
                         .setTitle(getString(R.string.not_register_alert))
                         .setMessage(getString(R.string.not_register_alert_message))
                         .setCancelable(false)
-                        .setPositiveButton(getString(R.string.not_register_alert_button), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                        .setPositiveButton(getString(R.string.delete), (dialog, which) -> {
 
-                                // サロゲートペアを検出した場合は、検出文字含む以後の文字列を全て削除する
+                            // サロゲートペアを検出した場合は、検出文字含めた以降の文字列を全て削除する
 
-                                eMemo.removeTextChangedListener(MainManagementStoreMemo.this);
+                            eMemo.removeTextChangedListener(MainManagementStoreMemo.this);
 
-                                text[0] = text[0].substring(0, deleteIndex);
-                                eMemo.setText(text[0]);
-                                eMemo.setSelection(text[0].length());
-                                CreateXML.updateText(mainApplication, memoTagNames[catchTappedPosition], text[0]);
+                            text[0] = text[0].substring(0, deleteIndex);
+                            eMemo.setText(text[0]);
+                            eMemo.setSelection(text[0].length());
+                            CreateXML.updateText(mainApplication, memoTagNames[catchTappedPosition], text[0]);
 
-                                eMemo.addTextChangedListener(MainManagementStoreMemo.this);
+                            eMemo.addTextChangedListener(MainManagementStoreMemo.this);
 
-                            }
                         })
                         .show();
                 return;
