9e4ab5ca2522c9148edf25df0387d62602f1de99
ishinotakkyuu <tanktopgentleman.secondseason@gmail.com>
2022-09-23 23:54:38 +0900

    Ver3.0.1 完成

--- app/src/main/java/delson/android/j_management_app/MainActivity.java
+++ app/src/main/java/delson/android/j_management_app/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     // 共有データ
     MainApplication mainApplication;
+
+    // R04.09.23 データ移行用
+    SharedPreferences counterDate;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +63,24 @@ public class MainActivity extends AppCompatActivity {
         }
         mainApplication.setDocument(document);
         ReadXML.readInfo(mainApplication);
+
+        // R04.09.23 mainApplicationからSharedPreferencesへの移行処理
+        counterDate = getSharedPreferences("CounterDate", MODE_PRIVATE);
+        editor = counterDate.edit();
+        if (counterDate.getBoolean("Migration",true)){
+            // 移行フラグが存在しなければ１度だけ以下の処理が走る
+            editor.putString("TotalGames", mainApplication.getTotalGames());
+            editor.putString("StartGames", mainApplication.getStartGames());
+            editor.putString("SingleBig", mainApplication.getSingleBig());
+            editor.putString("CherryBig", mainApplication.getCherryBig());
+            editor.putString("SingleReg", mainApplication.getSingleReg());
+            editor.putString("CherryReg", mainApplication.getCherryReg());
+            editor.putString("Cherry", mainApplication.getCherry());
+            editor.putString("Grape", mainApplication.getGrape());
+            // 移行フラグをfalseにしてデータ保存
+            editor.putBoolean("Migration",false);
+            editor.apply();
+        }
     }
 
     public void MainManagementStore(View view) {
